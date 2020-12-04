8bd9b1c8f83b0c91de982cce1d843cfe2cd02ae0
Olivier Deschênes <olivierdeschenes9@gmail.com>
2020-12-04 12:21:40 -0500

    ajout des points.

---
+++ Assets/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static GestionnairePersonnage;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        string tag = other.gameObject.tag;
+
+        if (EstPersonnage(tag))
+        {
+            Destroy(gameObject);
+
+            GestionnairePoints
+                .AddPoints((Personnage)Enum.Parse(typeof(Personnage), tag));   
+        }
+    }
+}
---
+++ Assets/Script/Points/GestionnairePoints.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static GestionnairePersonnage;
+
+public class GestionnairePoints : MonoBehaviour
+{
+    static Dictionary<Personnage, int> pointsPersonnages;
+
+    public void Start()
+    {
+        pointsPersonnages = new Dictionary<Personnage, int>();
+
+        foreach (Personnage personnage in Enum.GetValues(typeof(Personnage)))
+        {
+            pointsPersonnages.Add(personnage, 0);
+            UpdateUIPointage(personnage);
+        }
+    }
+
+    public static void AddPoints(Personnage personnage, int nbPoints = 1)
+    {
+        pointsPersonnages[personnage] += nbPoints;
+
+        UpdateUIPointage(personnage);
+    }
+
+    private static void UpdateUIPointage(Personnage personnage)
+    {
+        Debug.Log($"{personnage}: {pointsPersonnages[personnage]}");
+        GameObject.FindGameObjectWithTag($"{personnage}TextPoint")
+            .GetComponent<Text>().text = $"{personnage}: {pointsPersonnages[personnage]}";
+    }
+}
